c9d81ddf23e9a04a6f0470aa09dd137f1a55027e
Diego <diegolopesdld@gmail.com>
2022-07-03 03:46:43 -0300

    fix svg footer size

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,15 +5,17 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer>
-      <p style={{ textAlign: "center", color: "white" }}>
+      <p
+        style={{ textAlign: "center", color: "white", marginRight: "0.25rem" }}
+      >
         Desenvolvido por: Diego Lopes &copy;
       </p>
       <span className="links">
         <a href="https://www.linkedin.com/in/diegodld" target="_blank">
-          <FaLinkedin size={20} />
+          <FaLinkedin size={16} />
         </a>
         <a href="https://www.github.com/diegodld" target="_blank">
-          <FaGithub size={20} />
+          <FaGithub size={16} />
         </a>
       </span>
     </footer>
